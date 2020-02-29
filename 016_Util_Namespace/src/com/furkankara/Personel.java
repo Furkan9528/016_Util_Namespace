c613f1d81b1f16101611cf5d170dac90f9f4f130
Furkan9528 <Furkan9528@github.com>
2020-02-29 16:04:10 +0100

    Example of UtilNamespace

---
+++ 016_Util_Namespace/src/com/furkankara/Personel.java
@@ -0,0 +1,43 @@
+package com.furkankara;
+
+import java.util.*;
+
+public class Personel {
+
+	List<?> listemiz;
+	Set<?> setimiz;
+	//Map<?,?> ogrenciMap;
+	//Properties	propertymiz;
+	
+	//----------------------------------------------
+
+	public List<?> getListemiz() {
+		return listemiz;
+	}
+	public void setListemiz(List<?> listemiz) {
+		this.listemiz = listemiz;
+	}
+	public Set<?> getSetimiz() {
+		return setimiz;
+	}
+	public void setSetimiz(Set<?> setimiz) {
+		this.setimiz = setimiz;
+	}
+	
+	//----------------------------------------------
+
+	@Override
+	public String toString() {
+		return "Personel [listemiz=" + listemiz + ", setimiz=" + setimiz + "]";
+	}
+	
+	//----------------------------------------------
+
+	
+
+	
+	
+	
+	
+	
+}
---
+++ 016_Util_Namespace/src/com/furkankara/Sehir.java
@@ -0,0 +1,25 @@
+package com.furkankara;
+
+import java.util.Map;
+
+public class Sehir {
+
+	private Map <?,?> mapimiz;
+
+	public Map<?, ?> getMapimiz() {
+		return mapimiz;
+	}
+
+	public void setMapimiz(Map<?, ?> mapimiz) {
+		this.mapimiz = mapimiz;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Sehir [mapimiz=" + mapimiz + "]";
+	}
+	
+	
+	
+}
